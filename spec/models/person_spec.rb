5636842261d4cd3269b3dae56ef381aabdd1e3eb
Stanley <stanleyjsdz@gmail.com>
2015-02-28 19:21:39 -0700

    email_address scaffold generated

--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -19,4 +19,12 @@ RSpec.describe Person, type: :model do
     expect(person).not_to be_valid
   end
 
+  it 'has an array of phone numbers' do
+    expect(person.phone_numbers).to eq([])
+  end
+
+  it 'has an array of email addresses' do
+    expect(person.email_addresses).to eq([])
+  end
+
 end
