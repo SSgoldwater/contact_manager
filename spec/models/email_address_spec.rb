47707973d41855e93b682aab5dec09237be5aa52
Stanley <stanleyjsdz@gmail.com>
2015-02-28 20:18:50 -0700

    person view shows related emails

--- spec/models/email_address_spec.rb
+++ spec/models/email_address_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe EmailAddress, type: :model do
-  let(:email_address) { EmailAddress.new({ address: 'hi@abc.com' }) }
+  let(:email_address) { EmailAddress.new({ address: 'hi@abc.com', person_id: 2 }) }
 
   it 'is valid' do
     expect(email_address).to be_valid
@@ -12,5 +12,9 @@ RSpec.describe EmailAddress, type: :model do
     expect(email_address).to be_invalid
   end
 
+  it 'is invalid without a person_id' do
+    email_address.person_id = nil
+    expect(email_address).to be_invalid
+  end
 
 end
